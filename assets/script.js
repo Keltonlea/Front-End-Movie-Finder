5e0f4f5907520ab2748d96b1c0e1206669dd728b
Marty Hernandez <margarito.hernandezc@gmail.com>
2022-05-14 14:04:58 -0700

    cleaned up rating display

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var userMovie = document.getElementById('movie-input');
 var pastSearchButtons = document.querySelector("#past-search-buttons");
 var modalContainer = document.getElementById('#myModal');
 var movies = [];
-var pURL = "http://img.omdbapi.com/?"
+var pURL = "https://img.omdbapi.com/?"
 var pMovie = "i="
 var poster = "&h=600&"
 var pApi = "apikey=9279f439"
@@ -61,7 +61,7 @@ var pastSearchHandler = function (event) {
 
 
 //fetch
-var baseURL = "http://www.omdbapi.com/?";
+var baseURL = "https://www.omdbapi.com/?";
 var api = "apikey=9279f439&"
 var movie = "t="
 // var movieTitle = "John Wick"
@@ -74,9 +74,9 @@ function callMovie(userInput) {
             document.getElementById("list-group-item-A").innerHTML = "Movie Plot: " + JSON.stringify(data.Plot);
             document.getElementById("list-group-item-B").innerHTML = "Cast: " + JSON.stringify(data.Actors);
             document.getElementById("card-title").innerHTML = JSON.stringify(data.Title);
-            document.getElementById("list-group-item-C").innerHTML = "Internet Movie Database Rating: " + JSON.stringify(data.Ratings[0]);
-            document.getElementById("list-group-item-D").innerHTML = "Rotten Tomatoes Rating: " + JSON.stringify(data.Ratings[1]);
-            document.getElementById("list-group-item-E").innerHTML = JSON.stringify(data.Ratings[2]);
+            document.getElementById("list-group-item-C").innerHTML = "Internet Movie Database Rating: " + JSON.stringify(data.Ratings[0]).split('"')[7];
+            document.getElementById("list-group-item-D").innerHTML = "Rotten Tomatoes Rating: " + JSON.stringify(data.Ratings[1]).split('"')[7];
+            document.getElementById("list-group-item-E").innerHTML = "MetaCritic Rating: " + JSON.stringify(data.Ratings[2]).split('"')[7];
             moviePoster(data.imdbID);
             imdbTrailer(IbaseURL + data.imdbID);
         });
