f50855ec82e418af663401e12fcdc2f31bf2d5ac
Kelly Leach <101753828+Keltonlea@users.noreply.github.com>
2022-05-19 18:55:36 -0700

    updated design, poster image fixed, saved movie list fixed and search for movie buttons work

--- assets/script.js
+++ assets/script.js
@@ -11,9 +11,12 @@ var poster = "&h=600&"
 var pApi = "apikey=9279f439"
 var trailerModal = document.querySelector('.trailer');
 var closeTrailer = document.querySelector('.close-trailer');
-let searchHistory = JSON.parse(localStorage.getItem("movies")) || [];
 var iFrame = document.getElementById("myFrame");
-var clearBtnMovieList = document.querySelector('.clearSavedBtn');
+var commentContainer = document.getElementById("allComments");
+var searchModalBtn = document.getElementById("pleaseClose");
+var closeTrailer = document.querySelector(".closeTrailer")
+
+
 
 
 
@@ -34,41 +37,54 @@ var formSubmitHandler = function () {
         callMovie(movie);
         movies.unshift({ movie });
         movie.value = "";
+        pastSearch(movie);
 
-       
     } else {
         $('#myModal').modal('show');
         $('ul li').remove();
         $('#moviePoster').remove();
-        } 
+    }
     saveSearch();
-    pastSearch(movie);
 }
+
+
 
 //Function for saving search in local storage//
 var saveSearch = function () {
-    
+
     localStorage.setItem("movies", JSON.stringify(movies));
-    console.log("save search")
 };
+
 
 
 
 //generate movie search buttons//
 var pastSearch = function (pastSearch) {
-    for (let i=0; i<searchHistory.length; i++) {
+        pastSearchEl = document.createElement("button");
+        pastSearchEl.textContent = pastSearch;
+        pastSearchEl.classList = "d-flex w-100 btn-primary border p-2";
+        pastSearchEl.setAttribute("data-movie", pastSearch)
+        pastSearchEl.setAttribute("type", "submit");
+        pastSearchButtons.prepend(pastSearchEl);
 
-    pastSearchEl = document.createElement("button");
-    pastSearchEl.textContent = pastSearch;
-    pastSearchEl.classList = "d-flex w-100 btn-primary border p-2";
-    pastSearchEl.setAttribute("data-movie", pastSearch)
-    pastSearchEl.setAttribute("type", "submit", "text-center");
-    pastSearchButtons.prepend(pastSearchEl);
-    }}
-
-
+        localStorage.setItem("search", JSON.stringify(pastSearch))
+        console.log(pastSearch)
 
 
+}
+
+   
+
+   
+
+    var pastSearchHandler = function(event){
+        var movie = event.target.getAttribute("data-movie")
+        if(movie){
+            callMovie(movie)
+        }
+    }
+    
+    
 
 
 
@@ -76,9 +92,9 @@ var pastSearch = function (pastSearch) {
 //fetch
 
 function callMovie(userInput) {
-var baseURL = "https://www.omdbapi.com/?";
-var api = "apikey=9279f439&"
-var movie = "t="
+    var baseURL = "https://www.omdbapi.com/?";
+    var api = "apikey=9279f439&"
+    var movie = "t="
     fetch(baseURL + api + movie + userInput)
         .then(response => response.json())
         .then(data => {
@@ -90,8 +106,8 @@ var movie = "t="
             document.getElementById("list-group-item-C").innerHTML = "Internet Movie Database Rating: " + JSON.stringify(data.Ratings[0]).split('"')[7];
             document.getElementById("list-group-item-D").innerHTML = "Rotten Tomatoes Rating: " + JSON.stringify(data.Ratings[1]).split('"')[7];
             document.getElementById("list-group-item-E").innerHTML = "MetaCritic Rating: " + JSON.stringify(data.Ratings[2]).split('"')[7];
-            moviePoster(data.imdbID);
-            console.log(IbaseURL + data.imdbID);
+            moviePoster(data.imdbID, data.Poster);
+            // console.log(IbaseURL + data.imdbID);
             imdbTrailer(data.imdbID)
             imdbTrailer(IbaseURL + data.imdbID);
 
@@ -99,14 +115,24 @@ var movie = "t="
 }
 
 //Movie Poster Function//
-async function moviePoster(imdb) {
+async function moviePoster(imdb, thumbnailURL) {
 
     var response = await fetch(pURL + pMovie + imdb + poster + pApi)
     const imageBlob = await response.blob()
     var objectUrl = URL.createObjectURL(imageBlob);
     document.querySelector("#moviePoster").src = objectUrl;
-
+    if (response.status === 404) {
+        console.log("In IF condition");
+        document.querySelector("#moviePoster").src = thumbnailURL;
+    } else {
+        console.log("in ELSE Condition");
+        document.querySelector("#moviePoster").src = objectUrl;
+    }
+    console.log(response);
+    console.log(response.status);
 }
+
+
 
 
 //Movie Trailer Function//
@@ -117,9 +143,9 @@ function imdbTrailer(id) {
     fetch(imdbURL)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
-            console.log(data.linkEmbed)
-            console.log(document.getElementById("myFrame").src)
+            // console.log(data)
+            // console.log(data.linkEmbed)
+            // console.log(document.getElementById("myFrame").src)
             iFrame.setAttribute('src', data.linkEmbed)
         })
 }
@@ -127,45 +153,107 @@ function imdbTrailer(id) {
 
 
 
-var pastSearchHandler = function (event) {
-    var movie = event.target.getAttribute("data-movie")
-    if (movie) {
-        callMovie(movie);
-    }
-}
-
 
 //save movies to watch to local storage // 
-
-$(".saveBtn").on("click", function (){
-
-  
+$(".saveBtn").on("click", function () {
     let movieList = $(this).data("film");
     let value = $(this).siblings("textarea").val();
-    
-  
-   
-
-    console.log(movieList);
-    console.log(value);
+    // console.log(movieList);
+    // console.log(value);
     localStorage.setItem(movieList, value);
-
 });
 let textContent = $(".movie-list-form textarea");
-
-$(textContent).each(function(){
-
-
-  
+$(textContent).each(function () {
     var film = $(this).data("film");
-    
     var description = localStorage.getItem(film);
     $(this).val(description);
-  
-
 })
 
+$(".clearSavedBtn").on("click", function () {
 
+    let movieList = $(this).data("film");
+    let value = $(this).siblings("textarea").val();
+
+    localStorage.removeItem(movieList);
+    localStorage.removeItem(value);
+})
+
+function addComment(ev) {
+    let commentText;
+    var textBox = document.createElement('div');
+    textBox.className = "commentText";
+    var replyButton = document.createElement('button');
+    replyButton.className = 'reply'
+    replyButton.innerHTML = "Reply";
+    var likeButton = document.createElement("button");
+    likeButton.innterHTML = "Like";
+    likeButton.className = 'likecomment';
+    var deleteButton = document.createElement('button');
+    deleteButton.innerHTML = "Delete";
+    deleteButton.className = "deleteComment";
+    if (hasClass(ev.target.parentElement, 'container')) {
+        const wrapDiv = document.createElement('div');
+        wrapDiv.className = 'wrapper';
+        wrapDiv.style.marginLeft = 0;
+        commentText = document.getElementById('newComment').value;
+        document.getElementById('newComment').value = '';
+        textBox.innerHTML = commentText;
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+        commentContainer.appendChild(wrapDiv);
+    } else {
+        wrapDiv = ev.target.parentElement;
+        commentText = ev.target.parentElement.firstElementChild.value;
+        textBox.innerHTML = commentText;
+        wrapDiv.innerHTML = '';
+        wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+    }
+
+
+    // const wrapDiv = document.createElement("div");
+    // wrapDiv.className = "wrapper";
+    // wrapDiv.style.marginLeft = 0;
+    // commentText = document.getElementById("newComment").value;
+    // document.getElementById("newComment").value = '';
+    // textBox.innerHTML = commentText;
+    // wrapDiv.append(textBox, replyButton, likeButton, deleteButton);
+    // commentContainer.appendChild(wrapDiv);
+    setOnLocalStorage();
+}
+function hasClass(elem, className) {
+    return elem.className.split(' ').indexOf(className) > -1;
+}
+document.getElementById('allComments').addEventListener('click', function (e) {
+    if (hasClass(e.target, 'reply')) {
+        const parentDiv = e.target.parentElement;
+        const wrapDiv = document.createElement('div');
+        wrapDiv.style.marginLeft = (Number.parseInt(parentDiv.style.marginLeft) + 15).toString() + 'px';
+        wrapDiv.className = 'wrapper';
+        const textArea = document.createElement('textarea');
+        textArea.style.marginRight = '20px';
+        const addButton = document.createElement('button');
+        addButton.className = 'addReply';
+        addButton.innerHTML = 'Add';
+        const cancelButton = document.createElement('button');
+        cancelButton.innerHTML = 'Cancel';
+        cancelButton.className = 'cancelReply';
+        wrapDiv.append(textArea, addButton, cancelButton);
+        parentDiv.appendChild(wrapDiv);
+    } else if (hasClass(e.target, 'addReply')) {
+        addComment(e);
+    } else if (hasClass(e.target, 'likeComment')) {
+        const likeBtnValue = e.target.innerHTML;
+        e.target.innerHTML = likeBtnValue !== 'Like' ? Number.parseInt(likeBtnValue) + 1 : 1;
+    } else if (hasClass(e.target, 'cancelReply')) {
+        e.target.parentElement.innerHTML = '';
+    } else if (hasClass(e.target, 'deleteComment')) {
+        e.target.parentElement.remove();
+    }
+    setOnLocalStorage();
+});
+function setOnLocalStorage() {
+    localStorage.setItem('template', document.getElementById('allComments').innerHTML);
+}
+//save movies to watch to local storage // 
 
 
 
@@ -173,13 +261,19 @@ $(textContent).each(function(){
 
 document.getElementById('search-button').addEventListener('click', function (event) {
     event.preventDefault();
-   
+
     var userMovieChoice = document.getElementById('movie-input').value;
-   
+    
+
     // console.log(userMovie);
     callMovie(userMovieChoice)
     formSubmitHandler();
 
+})
+
+$(".modalBtn").click(function () {
+    $('#myModal').modal('hide')  ;
+    location.reload()
 })
 
 clearButton.addEventListener("click", function () {
@@ -192,28 +286,16 @@ clearButton.addEventListener("click", function () {
 
 })
 
-$(".clearSavedBtn").on("click", function (){
-
-  
-    let movieList = $(this).data("film");
-    let value = $(this).siblings("textarea").val();
-    
-  
-   
-
-   
-    localStorage.remove.te(movieList, value);
-
-});
-
-pastSearchButtons.addEventListener("click", pastSearchHandler);
+pastSearchButtons.addEventListener("click", pastSearchHandler)
 
 trailerModal.addEventListener("click", function () {
     $('#myTrailerModal').modal('show');
 })
 
 
-$(".close-trailer").click(function(){
-    $('#myTrailerModal').modal('hide')
+document.getElementById('addComments').addEventListener("click", function (ev) {
+    addComment(ev);
 });
 
+
+    
